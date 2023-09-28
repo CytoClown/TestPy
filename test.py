7cdd0a840b2b17e8f3ca2aa2bda802a895422119
CytoClovn <nickolayknz@gmail.com>
2023-09-28 23:12:47 +0300

    test

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hello world')
+import requests
+from pprint import pprint
+
+def test1():
+    url = 'https://www.advantageonlineshopping.com/catalog/api/v1/products/35'
+    response = requests.get(url)
+    pprint(response.json())
+    print()
+    print(f'Status code: {response.status_code}')
